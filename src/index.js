5dddd21b719e7efefb971c8d2fa52d8e639ab49d
Sam Pellegrene <pellegrenesam@gmail.com>
2016-10-27 11:58:57 -0500

    10.27.16 jvs class work

---
+++ src/index.js
@@ -0,0 +1,31 @@
+// for (var i=0; i < 5; i++ ){
+//   //  i=0
+//   // i=1
+//   if(i>2) {
+// console.log('watsup'+ i);
+// } else {
+//   console.log('DOGE'+ i);
+// }
+// };
+
+// bitcoinPrice = 370;
+//
+// while (bitcoinPrice < 400) {
+//   console.log('hold at price:' + bitcoinPrice);
+//
+// //  Made a call to an api that gave us the current bitcoinPrice
+//
+//   bitcoinPrice = bitcoinPrice +10;
+//
+// }
+//
+//   console.log('SELL at price:' + bitcoinPrice);
+    function reverse(s) 
+
+
+  var jellyBeanFlavors = ['grape', 'cherry', 'juicy pear'];
+  var newJellyBeanFlavors = []
+  jellyBeanFlavors.forEach(function(flavor) {
+    console.log('current flavor is ' + flavor);
+
+  });
